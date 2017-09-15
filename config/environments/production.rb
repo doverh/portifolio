86da7af829a9c2175963b4028b846a456387d6a6
doverh <doverheld@gmail.com>
2017-09-15 11:39:22 -0400

    configs contact form

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,13 +82,14 @@ config.action_mailer.default_url_options = { host: 'https://iamdover.herokuapp.c
   config.action_mailer.delivery_method = :smtp
   
   config.action_mailer.smtp_settings = {
-  address:              'smtp.sendgrid.net',
-  port:                 587,
-  domain:               'heroku.com',
-  user_name:            ENV['SENDGRID_USERNAME'],
-  password:             ENV['SENDGRID_PASSWORD'],
-  authentication:       'plain',
-  enable_starttls_auto: true  }
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :domain         => 'heroku.com',
+  :user_name      =>  ENV['SENDGRID_USERNAME'],
+  :password       =>  ENV['SENDGRID_PASSWORD'],
+  :authentication =>  'plain',
+  :enable_starttls_auto => true  
+}
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
