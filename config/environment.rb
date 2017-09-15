7abe7e4f9acd43ef07fe1c9e4bd0e73ae1ab821b
doverh <doverheld@gmail.com>
2017-09-15 12:14:30 -0400

    Send config email

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+  ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :user_name      =>  ENV['SENDGRID_USERNAME'],
+  :password       =>  ENV['SENDGRID_PASSWORD'],
+  :domain => 'https://iamdover.herokuapp.com/',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+
+}
