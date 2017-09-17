9be7c8ad40fb80fec782b8cc6b6dddf72abc074d
doverh <doverheld@gmail.com>
2017-09-16 21:07:00 -0400

    Send config email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,16 +77,16 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
 
-
+  config.action_mailer.default_url_options = { host: 'https://iamdover.herokuapp.com/' }
+  
   config.action_mailer.delivery_method = :smtp
   
   ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
+  :domain         => 'heroku.com',
   :user_name      =>  ENV['SENDGRID_USERNAME'],
   :password       =>  ENV['SENDGRID_PASSWORD'],
-  :domain => 'iamdover.herokuapp.com',
-  :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
