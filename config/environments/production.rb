a0bb19abc33343dc216a86d6cb2a978e4e899fea
doverh <doverheld@gmail.com>
2017-09-17 09:44:58 -0400

    Send config email again 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,15 +64,16 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'https://iamdover.herokuapp.com' }
  config.action_mailer.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
- :user_name => ENV['SENDGRID_USERNAME'],
- :password => ENV['SENDGRID_PASSWORD'],
- :domain => 'heroku.com',
- :address => 'smtp.sendgrid.net',
- :port => 587,
- :authentication => :plain,
- :enable_starttls_auto => true
- }
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "heroku.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+}
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
