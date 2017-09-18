50bf88d7d72cc08c932f83a142d1a361ea8effe6
doverh <doverheld@gmail.com>
2017-09-18 10:07:06 -0400

    Adjusts contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,17 +1,21 @@
 class ContactsController < ApplicationController
-
-def new 
- @contact = Contact.new
- end
+  def new
+    @contact = Contact.new
+  end
 def create
- @contact = Contact.new(params[:contact])
- @contact.request = request
- if @contact.deliver
- flash.now[:error] = nil
- redirect_to root_path, notice: 'Message sent successfully'
- else
- flash.now[:error] = 'Cannot send message'
- render :new
- end
- end
+    @contact = Contact.new contact_params
+if @contact.valid?
+      MessageMailer.contact(@contact).deliver_now
+      redirect_to root_path
+      flash[:notice] = "We have received your message and will be in touch soon!"
+    else
+      flash[:notice] = "There was an error sending your message. Please try again."
+      render :new
+    end
+  end
+
+private
+def contact_params
+    params.require(:message).permit(:name, :email)
+  end
 end
